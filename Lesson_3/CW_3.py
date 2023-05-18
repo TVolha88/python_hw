4010290246a44f0e28ff01726c4cd1a0961f0c54
TVolha88 <tvolha88@gmail.com>
2023-05-18 20:06:25 +0300

    my 2nd commit. HM

---
+++ Lesson_3/CW_3.py
@@ -0,0 +1,24 @@
+# print(5 == 5)
+# print(5 == 4)
+#
+# print(5 != 5)
+# print(5 != 4)
+#
+# print(5 > 5)
+# print(5 > 4)
+#
+# print(5 >= 5)
+# print(5 >= 4)
+
+# a=5
+# if a>=5:
+#     print('perfect')
+# else:
+#     print('think about it more')
+
+num = int(input("Enter the number:"))
+
+if num % 2 == 0:
+    print("четное число")
+else:
+    print("нечетное число")
