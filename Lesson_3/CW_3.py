885af31474119242783e6b7c99c6d52a3a1edeb3
TVolha88 <tvolha88@gmail.com>
2023-05-23 12:53:08 +0300

    Lesson_4

--- Lesson_3/CW_3.py
+++ Lesson_3/CW_3.py
@@ -1,24 +1,56 @@
-# print(5 == 5)
-# print(5 == 4)
+# # print(5 == 5)
+# # print(5 == 4)
+# #
+# # print(5 != 5)
+# # print(5 != 4)
+# #
+# # print(5 > 5)
+# # print(5 > 4)
+# #
+# # print(5 >= 5)
+# # print(5 >= 4)
 #
-# print(5 != 5)
-# print(5 != 4)
+# # a=5
+# # if a>=5:
+# #     print('perfect')
+# # else:
+# #     print('think about it more')
 #
-# print(5 > 5)
-# print(5 > 4)
+# num = int(input("Enter the number:"))
 #
-# print(5 >= 5)
-# print(5 >= 4)
-
-# a=5
-# if a>=5:
-#     print('perfect')
+# if num % 2 == 0:
+#     print("четное число")
 # else:
-#     print('think about it more')
+#     print("нечетное число")
+#
+# Задание No9
+#
+# Напишите программу, которая считывает строку и
+# проверяет является ли она словом 'Mister'
+#
+# string = input()
+# is_mister = (string == "Mister")
+# print(is_mister)
 
-num = int(input("Enter the number:"))
+# Задание No10
+#
+# Напишите программу, которая принимает на вход:
+# 1. Цвет доспехов(строка). Возможные варианты: red, yellow,
+# black
+# 2. Цвет щита(строка). Возможные варианты: red, yellow, black
+# Программа выводит True если цвет доспехов не красный и
+# цвет щита чёрный. В остальных случаях выводит False
 
-if num % 2 == 0:
-    print("четное число")
-else:
-    print("нечетное число")
+# armor_color = input("Enter the armor color: ")
+# shield_color = input("Enter the shield color: ")
+#
+# if armor_color != 'red' and shield_color == 'black':
+#     print(True)
+# else:
+#     print(False)
+
+armor_color = input("Enter the armor color: ")
+shield_color = input("Enter the shield color: ")
+
+is_knight = (armor_color!= 'red' and shield_color == 'black')
+print(is_knight)
