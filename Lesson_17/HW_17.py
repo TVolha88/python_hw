25967c3a243b620e4ea0c02546107447685558dd
TVolha88 <tvolha88@gmail.com>
2023-07-13 11:37:06 +0300

    Lesson_17.

--- Lesson_17/HW_17.py
+++ Lesson_17/HW_17.py
@@ -16,22 +16,24 @@ class Homework:
         elif isinstance(input_data, int):
             return self.process_number(input_data)
         else:
-            print("Ошибка: неподдерживаемый тип данных!")
+            print("Error: Check the type of the data!")
             return None
 
     def process_string(self, input_string):
         vowels = ['a', 'e', 'i', 'o', 'u']
-        consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-        vowel_product = 1
-        consonant_product = 1
+        consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q',
+                      'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+
+        vowel_pr = 1
+        consonant_pr = 1
 
         for char in input_string:
             if char.lower() in vowels:
-                vowel_product *= len(input_string)
+                vowel_pr *= len(input_string)
             elif char.lower() in consonants:
-                consonant_product *= len(input_string)
+                consonant_pr *= len(input_string)
 
-        if vowel_product <= len(input_string):
+        if vowel_pr <= len(input_string):
             return [char for char in input_string if char.lower() in vowels]
         else:
             return [char for char in input_string if char.lower() in consonants]
@@ -49,19 +51,3 @@ class Homework:
 
 homework = Homework()
 
-# Примеры использования
-input_data1 = "hello"
-result1 = homework.process_input(input_data1)
-print("Результат для строки 'hello':", result1)
-
-input_data2 = "python"
-result2 = homework.process_input(input_data2)
-print("Результат для строки 'python':", result2)
-
-input_data3 = 12345
-result3 = homework.process_input(input_data3)
-print("Результат для числа 12345:", result3)
-
-input_data4 = 2468
-result4 = homework.process_input(input_data4)
-print("Результат для числа 2468:", result4)
